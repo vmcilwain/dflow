a57356fbf11021c3c0e61e201d5d9f6ede156c75
Vell - [I am available to join projects.] <vmcilwain@me.com>
2020-12-21 21:49:24 -0500

    Update user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  context "db columns" do
+    should have_db_column(:first_name).of_type(:string)
+                                      .with_options(null: false, default: "")
+    should have_db_column(:last_name).of_type(:string)
+                                     .with_options(null: false, default: "")
+  end
+
+  context "validations" do
+    should validate_presence_of :first_name
+    should validate_presence_of :last_name
+  end
 end
