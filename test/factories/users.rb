bf4d9b19b71286ebbab0835e17aca0aca04be4f6
Vell - [I am available to join projects.] <vmcilwain@me.com>
2020-12-22 21:45:33 -0500

    Update users.rb

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    squence(:email) { |n| "user#{n}@example.com" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { "somepassword" }
     password_confirmation { "somepassword" }
     confirmed_at { Time.now }
