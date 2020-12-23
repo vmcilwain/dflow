c9ee536217c2ef9020d6cc4721c0e80a91653c6e
Vell - [I am available to join projects.] <vmcilwain@me.com>
2020-12-22 21:05:24 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module Dflow
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     
-    if Rails.env.staging?
-      Rails.application.config.credentials.content_path = Rails.root.join("config/credentials/staging.yml.enc")
+    if ENV["PIPE_ENV"].present?
+      Rails.application.config.credentials.content_path = Rails.root.join("config/credentials/#{ENV["PIPE_ENV"]}.yml.enc")
     end
   end
 end
