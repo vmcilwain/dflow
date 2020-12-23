4c10e6a2a0faa52acf5a0db41de15ef8d63b7cd5
Vell - [I am available to join projects.] <vmcilwain@me.com>
2020-12-23 13:17:12 -0500

    create roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_021646) do
+ActiveRecord::Schema.define(version: 2020_12_23_173158) do
+
+  create_table "roles", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "first_name", default: "", null: false
---
+++ test/models/role_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class RoleTest < ActiveSupport::TestCase
+  context "db columns" do
+    should have_db_column(:name).of_type(:string)
+                                .with_options(null: false, default: "")
+    should have_db_column(:description).of_type(:string)
+  end
+
+  context "validations" do
+    should validate_presence_of :name
+    should validate_uniqueness_of :name
+  end
+end
