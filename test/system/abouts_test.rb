a63d6b2447261b23fd5010ead7b9b6142cf8fc9e
Vell - [I am available to join projects.] <vmcilwain@me.com>
2021-01-02 16:10:57 -0500

    update tests for loggedin user

--- test/system/abouts_test.rb
+++ test/system/abouts_test.rb
@@ -1,7 +1,7 @@
 require "application_system_test_case"
 
 class AboutsTest < ApplicationSystemTestCase
-  test "visiting about without a title" do
+  test "as a visitor, I can  view about without a titler" do
     body = Faker::Lorem.words(number: 5).join("\s")
 
     create :about, body: body
@@ -9,11 +9,13 @@ class AboutsTest < ApplicationSystemTestCase
     visit about_path
     
     assert_not has_css? 'header h1'
+    assert_not has_link? :Create
+    assert_not has_link? :Edit
     assert has_link? :Home
     assert has_content? body
   end
 
-  test "visiting about with a title" do
+  test "as a visitor, I can view about with a title" do
     title = Faker::Lorem.words(number: 3).join("\s")
     body = Faker::Lorem.words(number: 5).join("\s")
 
@@ -22,7 +24,33 @@ class AboutsTest < ApplicationSystemTestCase
     visit about_path
     
     assert_selector 'header h1', text: title
+    assert_not has_link? :Create
+    assert_not has_link? :Edit
     assert has_link? :Home
     assert has_content? body
+  end
+
+  test "as an administrator, I have the create link available if there isn't an about previously created" do
+    new_session admin_user
+
+    visit about_path
+
+    assert has_link? :Create
+    assert_not has_link? :Edit
+  end
+
+  test "as an administrator, I have the Update link available if there is an about previously created" do
+    create :about
+
+    new_session admin_user
+
+    visit about_path
+
+    assert has_link? :Edit
+    assert_not has_link? :Create
+  end
+
+  test "creating an about " do
+    visit about_path
   end
 end
