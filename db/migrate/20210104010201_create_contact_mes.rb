7a3bd81e185e6ada043c956ddb69f74321ab80fe
Vell - [I am available to join projects.] <vmcilwain@me.com>
2021-01-04 18:40:37 -0500

    create contact_mes table

--- db/migrate/20210104010201_create_contact_mes.rb
+++ db/migrate/20210104010201_create_contact_mes.rb
@@ -1,6 +1,9 @@
 class CreateContactMes < ActiveRecord::Migration[6.1]
   def change
     create_table :contact_mes do |t|
+      t.string :name, null: false, default: ""
+      t.string :email, null: false, default: ""
+      t.text :message
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_200760) do
+ActiveRecord::Schema.define(version: 2021_01_04_010201) do
 
   create_table "abouts", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -54,6 +54,14 @@ ActiveRecord::Schema.define(version: 2020_12_23_200760) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "contact_mes", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.text "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "permissions", charset: "utf8mb4", force: :cascade do |t|
