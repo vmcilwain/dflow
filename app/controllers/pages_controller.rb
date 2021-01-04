da7e3059f66df961b6b6b370dbfa704308858da4
Vell - [I am available to join projects.] <vmcilwain@me.com>
2021-01-03 19:51:40 -0500

    update links now that there is a nav bar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,9 @@ class PagesController < ApplicationController
 
   def about
     @about = About.any? ? About.last : About.new
-    @links = [helpers.link_to(:Home, root_url)]
     
     if current_user&.has_role?(:administrator)
-      @links << if About.any?
-        helpers.link_to(:Edit, edit_about_path(About.last))
-      else
-        helpers.link_to(:Create, new_about_path)
-      end
+      @links = About.any? ? [helpers.link_to(:Edit, edit_about_path(About.last))] : [helpers.link_to(:Create, new_about_path)]
     end
 
   end
