594ba870ca1bd8aea229bf2af50662aa95e378eb
Vell - [I am available to join projects.] <vmcilwain@me.com>
2021-01-04 19:48:26 -0500

    Create contact_mes_test.rb

---
+++ test/system/contact_mes_test.rb
@@ -0,0 +1,15 @@
+require "application_system_test_case"
+
+class ContactMesTest < ApplicationSystemTestCase
+  test "as a visitor, I can create a contact me" do
+    visit new_contact_me_path
+
+    fill_in :contact_me_name, with: Faker::Name.name
+    fill_in :contact_me_email, with: Faker::Internet.email
+    fill_in :contact_me_message, with: Faker::Lorem.paragraph
+
+    click_button :Send
+
+    assert_success_alert
+  end
+end
