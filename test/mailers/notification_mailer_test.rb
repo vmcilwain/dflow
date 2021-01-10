3c91483a4cb7ca6e440ef2ca56ddc704d079d744
Vell - [I am available to join projects.] <vmcilwain@me.com>
2021-01-09 19:17:42 -0500

    add contact me email test

--- test/mailers/notification_mailer_test.rb
+++ test/mailers/notification_mailer_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class NotificationMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "contact me email" do
+    message = Faker::Lorem.paragraph
+    contact_me = create :contact_me, message: message
+
+    email = NotificationMailer.with(contact_me: contact_me).contact_me_email
+    
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ["from@example.com"], email.from
+    assert_equal [Rails.application.credentials.contact_me_to], email.to
+    assert_equal contact_me.name + " would like to contact you", email.subject
+
+    assert email.text_part.body.to_s.include?("Message from #{contact_me.name}(#{contact_me.email})")
+    assert email.text_part.body.to_s.include?(message)
+    
+    assert email.html_part.body.to_s.include?("Message from #{contact_me.name}(#{contact_me.email})")
+    assert email.html_part.body.to_s.include?(message)
+  end
 end
