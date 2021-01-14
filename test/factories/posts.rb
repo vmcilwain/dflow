5135e749bbfd007e9eb6cedc4df46ecebb1168b9
Vell - [I am available to join projects.] <vmcilwain@me.com>
2021-01-14 18:32:37 -0500

    set title to a string

--- test/factories/posts.rb
+++ test/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :post do
-    title { Faker::Lorem.words(number: 5) }
+    title { Faker::Lorem.words(number: 5).join("\s") }
     content { Faker::Lorem.paragraphs(number: 5).join("<br />") }
     association :creator, factory: :user
   end
