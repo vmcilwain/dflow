e88ecbe6c3ba8b680a1fdfcb3866e5de0b438355
Vell - [I am available to join projects.] <vmcilwain@me.com>
2021-01-27 17:41:04 -0500

    set content to use actiontext

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_rich_text :content
+  
   validates :title, :content, presence: true
 
   belongs_to :creator, class_name: :User, foreign_key: :created_by, optional: true
